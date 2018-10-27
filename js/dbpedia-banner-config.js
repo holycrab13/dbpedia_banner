ebf5caaa9d084bbe1ff6fbd662d37fbc6eeb3c17
Jan Forberg <jan.forberg@mailbox.tu-dresden.de>
2018-10-27 11:39:35 +0200

    changed config, adjusted main code accordingly

--- js/dbpedia-banner-config.js
+++ js/dbpedia-banner-config.js
@@ -1,32 +1,49 @@
-var BANNER_CONFIG = 
+var BANNER_CONFIG =
 {
-    "priority" : {
-        "newsapi.aylien.com" :  {
-            "icon" : "https://news-api-demo.s3.amazonaws.com/images/logo-header.png",
-            "caption" : "Find latest news about {0}",
-            "href" : "https://newsapi.aylien.com/demo#!/?published_at.start=NOW-30DAYS%2FDAY&amp;published_at.end=NOW&amp;entities.body.links.dbpedia%5B%5D={1}&amp;sort_by=recency&amp;ref=dbpedia",
-            "rank" : 200
-        }
-    },
+    "priority" : [
+      { "key" : "aylien", "rank" : 200 },
+
+    ],
 
     "context" : {
-        "www.wikidata.org": {
-            "icon" : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Wikidata-logo-en.svg/320px-Wikidata-logo-en.svg.png",
-            "caption" : "Edit {0} on Wikidata",
-            "href" : "{1}?ref=dbpedia",
-            "rank" : 150
-        },
-        "d-nb.info" :  {
-            "icon" : "http://www.dnb.de/SiteGlobals/StyleBundles/Bilder/DNB/logo-1.gif?__blob=normal&v=1",
-            "caption" : "DNB entry about {0}",
-            "href" : "{1}?ref=dbpedia",
-            "rank" : 99
-        },
-        "linkedgeodata.org" : {
-            "icon" : "http://linkedgeodata.org/files/lgdlogo.png",
-            "caption" : "{0} on LinkedGeoData",
-            "href" : "{1}?ref=dbpedia",
-            "rank" : 10
-        }
-    }
+        "http://www.w3.org/2002/07/owl#sameAs" :
+        [
+            { "key" : "wikidata", "rank" : 160 },
+            { "key" : "dnb", "rank" : 100 },
+            { "key" : "linkedgeodata" },
+        ],
+    },
+
+    "hosts" :
+    {
+      "aylien" :  {
+          "prefix" : "https://newsapi.aylien.com",
+          "icon" : "https://news-api-demo.s3.amazonaws.com/images/logo-header.png",
+          "caption" : "Find latest news about {0}",
+          "href" : "https://newsapi.aylien.com/demo#!/?published_at.start=NOW-30DAYS%2FDAY&amp;published_at.end=NOW&amp;entities.body.links.dbpedia%5B%5D={1}&amp;sort_by=recency&amp;ref=dbpedia",
+          "rank" : 200
+      },
+      "wikidata": {
+          "prefix" : "http://www.wikidata.org/",
+          "icon" : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Wikidata-logo-en.svg/320px-Wikidata-logo-en.svg.png",
+          "caption" : "Edit {0} on Wikidata",
+          "href" : "{1}?ref=dbpedia",
+          "rank" : 150
+      },
+      "dnb" :  {
+          "prefix" : "http://d-nb.info/",
+          "icon" : "http://www.dnb.de/SiteGlobals/StyleBundles/Bilder/DNB/logo-1.gif?__blob=normal&v=1",
+          "caption" : "Search the DNB for {0}",
+          "href" : "{1}?ref=dbpedia",
+          "rank" : 99
+      },
+      "linkedgeodata" : {
+          "prefix" : "http://linkedgeodata.org/",
+          "icon" : "http://linkedgeodata.org/files/lgdlogo.png",
+          "caption" : "Find {0} on LinkedGeoData",
+          "href" : "{1}?ref=dbpedia",
+          "rank" : 10
+      },
+    },
+
 };
--- js/dbpedia-banner.js
+++ js/dbpedia-banner.js
@@ -1,169 +1,211 @@
 
 $( document ).ready(function() {
 
-    // PROTOTYPE CODE: Get json URL from DOM for ajax request
-    var jsonURL = $("link[type='application/json']").attr("href");
+  // PROTOTYPE CODE: Get json URL from DOM for ajax request
+  var jsonURL = $("link[type='application/json']").attr("href");
 
-    // Returns the value from a list of object with a specified lang property
-    function getLanguageString(labels, lang) {
+  // Returns the value from a list of object with a specified lang property
+  function getLanguageString(labels, lang) {
 
-        if(labels == undefined) {
-            return null;
-        }
-
-        for(var i = 0; i < labels.length; i++) {
-
-            if(labels[i].lang == lang) {
-                return labels[i].value;
-            }
-        }
-
-        return null;
-    };
-
-    // Creates a banner DOM element from a banner item config object
-    function createBannerItem(itemConfig) {
-
-        return "<figure><img src='" + itemConfig.icon + "' height='72' width='auto'><figcaption><br><a target='_blank' href='" + 
-            itemConfig.href + "'>" + itemConfig.caption + "</a></figcaption></figure>";
+    if(labels == undefined) {
+      return null;
     }
 
-    function getHostName(urlString) {
+    for(var i = 0; i < labels.length; i++) {
 
-        if(typeof URL === "function") {
-
-            var url = new URL(urlString);
-            return url.hostname;
-        }
-
-        return null;
+      if(labels[i].lang == lang) {
+        return labels[i].value;
+      }
     }
 
-    // Get JSON asynchronously, this will be fetched from the DOM directly in a later version.
-    $.getJSON( jsonURL, function( data ) {
+    return null;
+  };
 
-        // Get the primary topic node
-        var primaryTopicSelector = $("head link[rel='foaf:primarytopic']");
+  function createBannerItem(configItem, host, labelEn, urlString) {
+    return {
+      icon : host.icon,
+      caption : host.caption.replace("{0}", labelEn),
+      href : host.href.replace("{1}", encodeURI(urlString)),
+      rank : (configItem.rank != undefined) ? configItem.rank : host.rank
+    }
+  }
 
-        if(primaryTopicSelector.length == 0) {
-            console.warn("Unable to find link with 'foaf:primarytopic' in the DOM");
-            return;
-        }
+  // Creates a banner DOM element from a banner item config object
+  function createDOMElement(bannerItem) {
 
-        // Get the resource URI        
-        var resourceURI = primaryTopicSelector.attr("href");
+    return "<figure><img src='" + bannerItem.icon + "' height='72' width='auto'><figcaption><br><a target='_blank' href='" +
+    bannerItem.href + "'>" + bannerItem.caption + "</a></figcaption></figure>";
+  }
 
-        if(resourceURI == null) {
-            console.warn("'href' attribute of 'foaf:primarytopic' link is not set");
-            return;
-        }
+  function getHostName(urlString) {
 
-        var bannerItems = [];
+    if(typeof URL === "function") {
 
-        var resourceObject = data[resourceURI];
+      var url = new URL(urlString);
+      return url.hostname;
+    }
 
-        if(resourceObject == null) {
-            console.warn("Resource information not found in JSON: owl:sameAs, rdf:label")
-            return;
-        }
+    return null;
+  }
 
-        // Get the list of rdf labels from the JSON object
-        var labels = resourceObject["http://www.w3.org/2000/01/rdf-schema#label"];
+  // Get JSON asynchronously, this will be fetched from the DOM directly in a later version.
+  $.getJSON( jsonURL, function( data ) {
 
-        // Get the english label
-        var labelEn = getLanguageString(labels, "en");
+    // Get the primary topic node
+    var primaryTopicSelector = $("head link[rel='foaf:primarytopic']");
 
-        if(labelEn == null) {
-            console.warn("Resource information not found in JSON: rdf:label")
-            return;
-        }
+    if(primaryTopicSelector.length == 0) {
+      console.warn("Unable to find link with 'foaf:primarytopic' in the DOM");
+      return;
+    }
 
-        // Get the sameAs relations for contextual banner items
-        var sameAsList = resourceObject["http://www.w3.org/2002/07/owl#sameAs"];
- 
-        // sameAs relations found and URL object is supported by the browser (not supported by IE11)
-        if(sameAsList != null) {
+    // Get the resource URI
+    var resourceURI = primaryTopicSelector.attr("href");
 
-            // Create contextual banner items
-            for(var same in sameAsList) {
+    if(resourceURI == null) {
+      console.warn("'href' attribute of 'foaf:primarytopic' link is not set");
+      return;
+    }
 
-                // Get the URL string, skip if not found
-                var urlString = sameAsList[same]["value"];
+    var bannerItems = [];
+    var bannerItemMap = {};
 
-                if(urlString == null) {
-                    continue;
-                }
+    var resourceObject = data[resourceURI];
 
-                // Get the host name
-                var hostName = getHostName(urlString);
+    if(resourceObject == null) {
+      console.warn("Resource information not found in JSON: owl:sameAs, rdf:label")
+      return;
+    }
 
-                if(BANNER_CONFIG["context"][hostName] != null) {
+    // Get the list of rdf labels from the JSON object
+    var labels = resourceObject["http://www.w3.org/2000/01/rdf-schema#label"];
 
-                    var bannerItem = BANNER_CONFIG["context"][hostName];
-                    bannerItem.href = bannerItem.href.replace("{1}", encodeURI(urlString));
-                    bannerItem.caption = bannerItem.caption.replace("{0}", labelEn);
+    // Get the english label
+    var labelEn = getLanguageString(labels, "en");
 
-                    bannerItems.push(bannerItem);
-                }
+    if(labelEn == null) {
+      console.warn("Resource information not found in JSON: rdf:label")
+      return;
+    }
+
+    // Get all context predicates from the config file
+    var contextConfig = BANNER_CONFIG["context"];
+
+    // Get a list of all the hosts set up for banner items
+    var hosts = BANNER_CONFIG["hosts"];
+
+    // Iterate over all predicates registered as context item
+    for(predicate in contextConfig) {
+
+      // Get the predicate config
+      var contextPredicateConfig = contextConfig[predicate];
+
+      // Get entities from the resource object
+      var contextEntities = resourceObject[predicate];
+
+      // Check whether there are any entities in the current document
+      if(contextEntities != undefined) {
+
+        // Iterate over the entitites
+        for(var entity in contextEntities) {
+
+          // Get the URL string, skip if not found
+          var urlString = contextEntities[entity]["value"];
+
+          if(urlString == null) {
+            continue;
+          }
+
+          // Iterate over all entries of the current predicate config
+          for(i in contextPredicateConfig) {
+
+            // Get the key of the config item
+            var key = contextPredicateConfig[i].key;
+
+            // Check, whether there is already a banner item registered with this key
+            if(bannerItemMap[key] != undefined) {
+              continue;
             }
-        }
 
-        // Create priority banner items
-        for(p in BANNER_CONFIG["priority"]) {
-            
-            var bannerItem = BANNER_CONFIG["priority"][p];
-            bannerItem.href = bannerItem.href.replace("{1}", encodeURI(resourceURI));
-            bannerItem.caption = bannerItem.caption.replace("{0}", labelEn);
+            // Get the hosts from the host config by key
+            var host = hosts[key];
 
-            bannerItems.push(bannerItem);
-        }
+            // Check, whether the current entity starts with any of the hosts' prefixes
+            if(urlString.startsWith(host.prefix)) {
 
-        // Sort banner items descending by rank
-        bannerItems.sort(function(a, b) {
-            return b.rank - a.rank;
-        });
+              // Add banner item, add entry to map
+              bannerItems.push(createBannerItem(contextPredicateConfig[i], host, labelEn, urlString));
+              bannerItemMap[key] = 1;
 
-
-        // Create DOM elements and append to placeholder
-        for(b in bannerItems) {
-            $(".banner-slider").append(createBannerItem(bannerItems[b]));
-        }
-
-        // Create the tiny slider
-        var slider = tns({
-            "container": ".banner-slider",
-            "items": 2,
-            "slideBy": "page",
-            "mouseDrag": true,
-            "swipeAngle": false,
-            "speed": 400,
-            "nav": false,
-            "controls": false,
-            "gutter": 0,
-            "autoplay": true,
-            "autoplayButtonOutput": false,
-            "autoplayButton": false,
-            // container: '.my-slider',
-            // items: 1,
-            responsive: {
-                640: {
-                    edgePadding: 20,
-                    gutter: 20,
-                    items: 2
-                },
-                700: {
-                    gutter: 30
-                },
-                900: {
-                    items: 4
-                }
             }
-        });
+          }
+        }
+      }
+    }
 
+    var priorityConfig = BANNER_CONFIG["priority"];
 
-    }).fail(function() {
-        console.log( "error" );
+    // Create priority banner items
+    for(i in priorityConfig) {
+
+      var key = priorityConfig[i].key;
+
+      if(bannerItemMap[key] != undefined) {
+        continue;
+      }
+
+      var host = hosts[key];
+
+      bannerItems.push(createBannerItem(priorityConfig[i], hosts[key], labelEn, resourceURI));
+
+      bannerItemMap[key] = 1;
+    }
+
+    // Sort banner items descending by rank
+    bannerItems.sort(function(a, b) {
+      return b.rank - a.rank;
     });
 
-});
 
+    // Create DOM elements and append to placeholder
+    for(b in bannerItems) {
+      $(".banner-slider").append(createDOMElement(bannerItems[b]));
+    }
+
+    // Create the tiny slider
+    var slider = tns({
+      "container": ".banner-slider",
+      "items": 2,
+      "slideBy": "page",
+      "mouseDrag": true,
+      "swipeAngle": false,
+      "speed": 400,
+      "nav": false,
+      "controls": false,
+      "gutter": 0,
+      "autoplay": true,
+      "autoplayButtonOutput": false,
+      "autoplayButton": false,
+      // container: '.my-slider',
+      // items: 1,
+      responsive: {
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 2
+        },
+        700: {
+          gutter: 30
+        },
+        900: {
+          items: 4
+        }
+      }
+    });
+
+
+  }).fail(function() {
+    console.log( "error" );
+  });
+
+});
